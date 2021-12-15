832c0498830da6856ac38fec6ba083345b331759
Scott McClung <scott.mcclung@gmail.com>
2021-12-15 10:04:01 -0500

    Try to update the webapp with a commit

--- app.py
+++ app.py
@@ -4,3 +4,15 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello, World!"
+
+@app.route("/thao")
+def thao():
+    return "Hello, Thao!"
+
+@app.route("/tyler")
+def tyler():
+    return "Hello, TJ!"
+
+@app.route("/maddy")
+def maddy():
+    return "Hello, Maddy!"
